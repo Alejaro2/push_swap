4645698366331e9b0035855b6175cd350364f9f5
Alejandro Rodriguez <alejaro2@c2r15s3.42madrid.com>
2025-02-11 21:08:43 +0100

    hola

---
+++ Makefile
@@ -0,0 +1,51 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: alejaro2 <alejaro2@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/01/13 16:31:47 by alejaro2          #+#    #+#              #
+#    Updated: 2025/02/11 19:33:09 by alejaro2         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+
+NAME = push_swap
+
+CC = cc 
+
+CFLAGS = -Wall -Wextra -Werror -g
+
+LIBFT_DIR =libft 
+LIBFT = $(LIBFT_DIR)/libft.a 
+
+RM = rm -f
+
+SRC = main.c parse_convert.c parse.c validate.c utils.c
+
+OBJS = $(SRC:.c=.o)
+
+all : $(LIBFT) $(NAME)
+
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -L$(LIBFT_DIR) -lft -o $(NAME)
+
+
+$(LIBFT):
+	@make -C $(LIBFT_DIR)
+
+clean:
+	$(RM) $(OBJS)
+	make -C $(LIBFT_DIR) clean
+
+fclean: clean
+	$(RM) $(NAME)
+	make -C $(LIBFT_DIR) fclean
+
+re: fclean all
+
+.PHONY: all clean fclean re
+
+
---
+++ push_swap.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejaro2 <alejaro2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 16:31:30 by alejaro2          #+#    #+#             */
+/*   Updated: 2025/02/11 19:12:49 by alejaro2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+typedef struct s_node
+{
+	int value;
+	struct s_node *next;
+} t_node;
+
+typedef struct s_stack {
+	t_node *top;
+	int size;
+} t_stack;
+
+
+int is_valid_char(char c, int position);
+int valid_digits(int argc, char **argv);
+int valid_limits(char *str);
+void validate(char *split_str);
+void duplicate(int *numbers, int count);
+int is_sorted(int *numbers, int count);
+
+void ft_free_mem(char **array);
+int *save_mem(int count);
+
+void convert_numbers(char **split, int *numbers, int *new);
+int *convert_init(int argc, char **argv, int count);
+
+void ft_error(char *str, int flag);
+int is_all_spaces(char *str);
+
+t_node *create_node(int value);
+t_stack *create_stack(void);
+void lstadd(t_stack *stack, int value);
+
+#endif
